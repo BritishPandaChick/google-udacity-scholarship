bb3be4b6ac117602b35e72d0a655ab82ce79ff7e
BritishPandaChick <sbartley1080@gmail.com>
2018-01-14 20:43:45 -0500

    Add JS challenges from lesson 11

---
+++ all-tied-up.js
@@ -0,0 +1,6 @@
+/*
+ * Programming Quiz: All Tied Up (2-5)
+ */
+
+var joke = /* write your joke here */ "Why couldn\'t the shoes go out and play?\n They were all \"tied\" up!"
+console.log(joke);
---
+++ favorite-food.js
@@ -0,0 +1,5 @@
+/*
+ * Programming Quiz: Favorite Food (2-3)
+ */
+
+console.log(/* replace this comment with your string */ "Potstickers");
