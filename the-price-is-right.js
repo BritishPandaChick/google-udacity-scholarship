a870b45d38c58611fb8f0c347ab81be5331d1c6b
BritishPandaChick <sbartley1080@gmail.com>
2018-01-22 17:18:53 -0500

    add arrays such as udaciFamily, building the crew, and price is right

---
+++ the-price-is-right.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: The Price is Right (6-3)
+ */
+
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+
+// your code goes here
+prices[0] = 2.23;
+prices[2] = 9.11;
+prices[6] = 3.00;
+
+console.log(prices);
