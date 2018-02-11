0d3aeff4ce0eebb7b4cb3c7aa6adebe37d295dd0
BritishPandaChick <sbartley1080@gmail.com>
2018-02-11 15:39:39 -0500

    Add function for selecting size input

--- project-pixel-art-maker-starter-master/designs.js
+++ project-pixel-art-maker-starter-master/designs.js
@@ -1,12 +1,20 @@
 // Select color input
-$("#colorPicker").on("change", function() {
-  $("#input").css("color", $("#color").val());
-});
+
 // Select size input
-var value = $(this).val();
-if(value.length > 0) {
-  
-}
+$('#sizePicker').submit(function makeGrid(grid){
+  var rowInput = $('#input_height').val();
+  var colInput = $('#input_width').val();
+
+  for (var i = 1; i < rowInput; i++) {
+    $('table').append('<tr></tr>');
+
+    for (var j = 1; j <= colInput; j++){
+      $('tr:last').append('<td></td>');
+      $('td').attr('class', 'cells');
+    }
+  }
+  grid.preventDefault();
+});
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
