0b625c7d50d87bcf9b7c8bea395891b447feba09
BritishPandaChick <sbartley1080@gmail.com>
2018-02-11 15:34:27 -0500

    Add pixel art project. Add link to jQuery

---
+++ project-pixel-art-maker-starter-master/designs.js
@@ -0,0 +1,16 @@
+// Select color input
+$("#colorPicker").on("change", function() {
+  $("#input").css("color", $("#color").val());
+});
+// Select size input
+var value = $(this).val();
+if(value.length > 0) {
+  
+}
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+
+// Your code goes here!
+
+}
