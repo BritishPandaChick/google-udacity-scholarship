7e3c7ea3d1a80d982478d148380cb74e37999dd7
BritishPandaChick <sbartley1080@gmail.com>
2018-02-12 19:04:37 -0500

    Change size and color inputs to variables. Added for loops for makeGrid function

--- project-pixel-art-maker-starter-master/designs.js
+++ project-pixel-art-maker-starter-master/designs.js
@@ -1,28 +1,34 @@
+$(function (){
+
 // Select color input
-$('.cells').click(function (event) {
-  var paint = $('#colorPicker').val();
-  $(event.target).css('background-color', paint);
-});
-
+  var colorPicker = $('#colorPicker');
 // Select size input
-$('#sizePicker').submit(function makeGrid(grid){
-  var rowInput = $('#input_height').val();
-  var colInput = $('#input_width').val();
-
-  for (var i = 1; i < rowInput; i++) {
-    $('table').append('<tr></tr>');
-
-    for (var j = 1; j <= colInput; j++){
-      $('tr:last').append('<td></td>');
-      $('td').attr('class', 'cells');
-    }
-  }
-  grid.preventDefault();
-});
+  var heightInput = $('#inputHeight');
+  var weightInput = $('#inputWeight');
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+  canvas.find('tbody').remove();
+
+  //submit button size changes to fit grid size
+  var gridRows = heightInput.val();
+  var gridCol = weightInput.val();
+
+  //tbody set to the table
+  canvas.append('<tbody></tbody>');
+
+  var canvasBody = canvas.find('tbody');
+
+  //drawing grid rows
+  for (var i = 0; i < gridRows; i++) {
+    canvasBody.append('<tr></tr>');
+  }
+
+  //draw grid col
+  for (var j = 0; j < gridCol; j++) {
+    canvas.find('tr').append('<td class="transparent"></td>');
+  }
+}
 
 // Your code goes here!
-
-}
+});
