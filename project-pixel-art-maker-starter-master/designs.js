2b9509f2d8f00f1659aea5834f6981e98b7924c2
BritishPandaChick <sbartley1080@gmail.com>
2018-02-13 16:43:44 -0500

    Add if-else statement to toggle grid color.

--- project-pixel-art-maker-starter-master/designs.js
+++ project-pixel-art-maker-starter-master/designs.js
@@ -7,28 +7,49 @@ $(function (){
   var weightInput = $('#inputWeight');
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-  canvas.find('tbody').remove();
+  function makeGrid() {
+    canvas.find('tbody').remove();
 
   //submit button size changes to fit grid size
-  var gridRows = heightInput.val();
-  var gridCol = weightInput.val();
+    var gridRows = heightInput.val();
+    var gridCol = weightInput.val();
 
   //tbody set to the table
-  canvas.append('<tbody></tbody>');
+    canvas.append('<tbody></tbody>');
 
-  var canvasBody = canvas.find('tbody');
+    var canvasBody = canvas.find('tbody');
 
   //drawing grid rows
-  for (var i = 0; i < gridRows; i++) {
-    canvasBody.append('<tr></tr>');
-  }
+    for (var i = 0; i < gridRows; i++) {
+      canvasBody.append('<tr></tr>');
+    }
 
   //draw grid col
-  for (var j = 0; j < gridCol; j++) {
-    canvas.find('tr').append('<td class="transparent"></td>');
+    for (var j = 0; j < gridCol; j++) {
+      canvas.find('tr').append('<td class="transparent"></td>');
+    }
   }
-}
 
-// Your code goes here!
+//update grid when submit button clicked
+  $(document).ready(function(){
+    $('input[type="submit"]').on('click', function(e){
+      e.preventDefault();
+      makeGrid();
+    });
+
+  //toggle grid color
+    $('#pixel_canvas').on('click', 'td', function(e){
+      var color = colorPicker.val();
+      var currentColor = $(this).css('background-color');
+      console.log(currentColor + ' color:' + color);
+
+      if ($(this).hasClass('transparent')) {
+        $(this).toggleClass('transparent');
+        $(this).css('background-color', color);
+      } else {
+        $(this).toggleClass('transparent');
+        $(this).css('background-color', 'transparent');
+      }
+    })
+  });
 });
