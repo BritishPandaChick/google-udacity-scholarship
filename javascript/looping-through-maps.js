308ad9dde77795fa2341d53de55aaa74ab14e2dc
BritishPandaChick <sbartley1080@gmail.com>
2018-07-01 16:31:10 -0400

    add looping through maps quiz

---
+++ javascript/looping-through-maps.js
@@ -0,0 +1,14 @@
+/*
+ * Using array destructuring, fix the following code to print the keys and values of the `members` Map to the console.
+ */
+
+const members = new Map();
+
+members.set('Evelyn', 75.68);
+members.set('Liam', 20.16);
+members.set('Sophia', 0);
+members.set('Marcus', 10.25);
+
+for (const [key, value] of members) {
+    console.log(key, value);
+}
